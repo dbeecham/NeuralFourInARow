2a9f924550c4e2d8b727370c6bbc5d9ee01898f9
Daniel Beecham <daniel@lunix.se>
2015-12-01 15:50:06 +0100

    Few insignificant changes to Square 'testing' suite.

--- Square.hs
+++ Square.hs
@@ -1,3 +1,5 @@
+module Square where
+
 import Genetics
 import Neural
 import Utilities
@@ -16,20 +18,16 @@ trainingSet = [ ((1><3)[1, 1, 1], (6><1)[1, 1, 1, 0, 1, 1])
 randompopulation :: IO [Genotype]
 randompopulation = do
     g <- getStdGen
-    return $ map nflatten $ take 10 $ randomNetworks g [3, 4, 5, 6]
+    return $ map nflatten $ take 10 $ randomNetworks g [3, 6]
 
 fit :: Genotype -> Double
-fit = (fitness trainingSet) . (nfromList [3, 4, 5, 6])
-
--- Iterates fitness and selection on a population
-iterations :: Population -> [Population]
-iterations population = iterate (strangeSelection fit) population
+fit = (fitness trainingSet) . (nfromList [3, 6])
 
 -- Iterates fitness and selection on a population, where the
 -- selection function uses randomness.
-randomIterations :: RandomGen g => g -> Population -> [Population]
-randomIterations g population = 
-    map fst $ iterate (rouletteWheel fit) (population, g)
+iterations :: RandomGen g => g -> Population -> [Population]
+iterations g population = 
+    map fst $ iterate (mySelection fit) (population, g)
 
 maximums :: [Population] -> [Double]
 maximums populations = map maximum $ (map.map) fit $ populations
@@ -37,11 +35,7 @@ maximums populations = map maximum $ (map.map) fit $ populations
 -- example to see how well we are doing
 example :: IO [Double]
 example = do
+    g <- newStdGen
     population <- randompopulation
-    return $ maximums (iterations population)
+    return $ maximums (iterations g population)
 
-main :: IO ()
-main = do
-    g <- getStdGen
-    let population = map nflatten $ take 10 (randomNetworks g [3, 4, 5, 6])
-    return ()
